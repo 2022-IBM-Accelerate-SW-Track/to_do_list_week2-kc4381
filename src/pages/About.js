034c80adf5b014fea282d40fa0ef76a70bd3a014
k1arac <90221309+k1arac@users.noreply.github.com>
2022-06-20 18:31:43 -0400

    second commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -18,10 +18,19 @@ export default class About extends Component {
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Kiara</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+              <ul>
+                <li> Hi! I'm Kiara and I'm from Long Island, New York </li>
+
+                Interests: 
+                <li> I like working out! </li>
+                <li> I enjoy reading books and listening to music! </li>
+
+                Fun fact: 
+                <li> I have a boston terrier and his name is Milo! </li>
+
+              </ul>
             </div>
           </div>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,18 @@ class Home extends Component {
       todos: [],
     };
   }
+
+  //the delete function 
+  deleteTodo = (id) =>{
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id; 
+    });
+    this.setState({
+      todos: todos,
+    }); 
+
+  }; 
+
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
   // updates the state with the new list.
   addTodo = (todo) => {
@@ -19,14 +31,28 @@ class Home extends Component {
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
-    todo.id = Math.random();
-    // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
-    // Update the local state with the new array.
-    this.setState({
-      todos: new_list,
-    });
+
+    //to check if a task already exists before performing the action to add an item
+    //let exist = this.state.todos.find(t => t.content.toLowerCase() === todo.content)
+    
+    //todo.id = Math.random();
+    if(this.state.todos.find(t=>t.content.trim().toLowerCase() === todo.content.trim().toLowerCase())){
+      return;
+    }
+    else{
+      todo.id = Math.random();
+      // Create a array that contains the current array and the new todo item
+      let new_list = [...this.state.todos, todo];
+
+      // Update the local state with the new array.
+      this.setState({
+        todos: new_list,
+      });
+
+
+    }  
   };
+
   render() {
     return (
       <div className="Home">
@@ -36,7 +62,8 @@ class Home extends Component {
         <AddTodo addTodo={this.addTodo} />
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo = {this.deleteTodo} />
+      
       </div>
     );
   }
